df27f473c44b899bf3887925bce2d34bf1520365
Riccardo Volonterio <Riccardo.Volonterio@appway.com>
2019-04-01 17:06:42 +0200

    Working server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-const volo = 5;
+import express from "express";
+
+const app = express();
+
+app.use(function(req, res, next) {
+  console.log(`Request: ${req.path}`);
+  next();
+});
+
+app.get("/", function(req, res) {
+  res.send("Hello World");
+});
+
+app.get("/todos", function(req, res) {
+  const todosJson = require("../assets/todo.json");
+  res.json(todosJson);
+});
+
+app.get("/todo/:id", function(req, res) {
+  throw new Error("Implement me");
+});
+
+console.log("Server starting");
+app.listen(3000, () => {
+  console.log("Server started");
+});
